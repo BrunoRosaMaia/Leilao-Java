2cec3189119749b95005a8936a1f1f21e1173c5c
BrunoRosaMaia <brunorosamaia@gmail.com>
2025-03-22 17:24:47 -0300

    Funcionalidade de salvar implementada

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -13,6 +13,7 @@ import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.sql.SQLException;
 
 
 public class ProdutosDAO {
@@ -22,12 +23,31 @@ public class ProdutosDAO {
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
     
-    public void cadastrarProduto (ProdutosDTO produto){
+    public boolean cadastrarProduto (ProdutosDTO produto){
         
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+        String sql = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
+        try {
+            conn = new conectaDAO().connectDB();
+            prep = conn.prepareStatement(sql);
+            prep.setString(1, produto.getNome());
+            prep.setInt(2, produto.getValor());
+            prep.setString(3, produto.getStatus());
+            prep.executeUpdate();
+            return true;
+        }
+        catch (SQLException erro){
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar produto: " + erro.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+            return false;
+        }
+        finally{
+            try {
+                if(prep != null) prep.close();
+                if(conn != null) conn.close();
+            }
+            catch (SQLException e){
+                e.printStackTrace();
+            }
+        }
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
